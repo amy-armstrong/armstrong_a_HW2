297c6dcfa80435e0eee6b8ec960ce9bdb8766387
amy-armstrong <a_armstrong148615@fanshaweonline.ca>
2026-03-05 20:43:20 -0500

    Javascript added
    In this commit, I added the Javascript

---
+++ js/counter.js
@@ -0,0 +1,94 @@
+export class Counter {
+    constructor(selector, initialValue=0) {
+        this.count = initialValue;
+        this.selector = selector;
+        this.mount();
+    }
+
+    mount() {
+        const container = document.querySelector(this.selector);
+        
+        this.display = document.createElement('div');
+        this.button = document.createElement('button');
+
+        // add buttons, decrement, reset
+        // this.decrement
+        // this.reset
+
+        // set button text
+        this.button.textContent = "increment";
+        // set new button text
+        // this.decrement...
+        // this.reset...
+
+        //append display and button into container div
+        container.appendChild(this.display);
+        container.appendChild(this.button);
+        // append new buttons
+        // container.appeandchild(new buttons)
+
+        //add event listener
+        this.button.addEventListener("click", ()=> this.increment());
+        // add new event listeners
+
+        // when this first gets mounted update the display
+        this.update();
+    }
+
+    // state methods
+    increment() {
+        this.count++;
+        this.update();
+    }
+
+        // decrement() {
+            // if statement to prevent lower than 0
+            // this.count--;
+            // this.update();
+        // }
+        
+        // reset() {
+            // this.count = 0;
+        // }
+
+    update() {
+        // set initial display content
+        this.display.textContent = `Count: ${this.count}`;
+    
+        //classList.toggle() this.count===0
+    }
+}
+
+//Counter is Super Class
+//StepCounter is subclass
+
+class StepCounter extends Counter {
+    counstructor(selector, initialValue = 0) {
+        super(selector, initialValue);
+        //add step property
+        //this.step = step;
+
+    }
+    //increment(){}
+    //decrement(){}
+}
+
+//homework notes
+//when counter goes to 0 add a class that will prevent user from dropping it to zero or resetting, as 0 is the base state
+//design a full webpage that documents that component
+//make website explain counter and how to use it
+//create a new stepper class using inheritance
+//takes a parameter called step which will dictate how much the counter will increment by
+//minimum of 10 commits
+//branch for each feature
+//branch commits spaced out
+//repo should show development
+
+//repo should show:
+//week 8 skeleton must be finished
+//week 9 core logic must be finished
+//week 10 must be finished
+//PROGRESSION
+//NO VIBE CODING
+
+//someone should be able to go to site and learn how it is done so that they can add it to their project
---
+++ js/main.js
@@ -0,0 +1,8 @@
+import { Counter } from './counter.js';
+
+const counter1 = new Counter('#counter-container1', 10);
+const counter2 = new Counter('#counter-container2');
+const counter3 = new Counter('#counter-container3');
+
+
+// counter step10 = new stepCounter(id, starting number, step)
